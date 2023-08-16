e7b47039159fbc93e4e9fe354305ec8c9ddcc7f5
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2023-08-16 21:20:57 +0530

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,20 @@
+import os 
+import cv2
+path = "Image"
+# Create an empty list named "Images"
+Images = []
+
+# Adding elements to the list
+Images.append("image111.jpg")
+Images.append("image112.jpg")
+Images.append("image113.jpg")
+Images.append("image114.jpg")
+Images.append("image115.jpg")
+Images.append("image116.jpg")
+Images.append("image117.jpg")
+Images.append("image118.jpg")
+Images.append("image119.jpg")
+Images.append("image120.jpg")
+
+# Print the list to see its contents
+print(Images)
